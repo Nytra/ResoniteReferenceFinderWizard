db7357108dd134138d8458fc0de775214e1d7db3
Nytra <14206961+Nytra@users.noreply.github.com>
2024-04-09 15:04:18 +0100

    Remove old unused code

--- ReferenceFinder/ReferenceFinder.cs
+++ ReferenceFinder/ReferenceFinder.cs
@@ -43,9 +43,9 @@ namespace ReferenceFinder
 			{
 				return new ReferenceFinderWizard(x);
 			}
+
 			Slot WizardSlot;
 
-			//eadonly ReferenceField<Slot> searchRoot;
 			readonly ReferenceField<IWorldElement> elementField;
 
 			readonly ValueField<bool> processWorkerSyncMembers;
@@ -55,20 +55,11 @@ namespace ReferenceFinder
 			readonly ValueField<bool> ignoreSelfReferences;
 			readonly ValueField<bool> ignoreSlotParentRef;
 			readonly ValueField<bool> showDetails;
-			//readonly ValueField<bool> confirmDestroy;
-			//readonly ValueField<string> nameField;
-			//readonly ValueField<bool> matchCase;
-			//readonly ValueField<bool> allowChanges;
-			//readonly ValueField<bool> searchNiceName;
 			readonly ValueField<int> maxResults;
-			//readonly ValueField<bool> exactMatch;
 
 			readonly ReferenceMultiplexer<ISyncRef> results;
 
-			//readonly Button destroyButton;
 			readonly Button searchButton;
-			//readonly Button enableButton;
-			//readonly Button disableButton;
 
 			bool performingOperations = false;
 
@@ -130,29 +121,11 @@ namespace ReferenceFinder
 
 			bool ValidateWizard()
 			{
-				//if (searchRoot.Reference.Target == null)
-				//{
-				//	UpdateStatusText("No search root provided!");
-				//	return false;
-				//}
-
-				//if (componentField.Reference.Target == null)
-				//{
-				//UpdateStatusText("No component provided!");
-				//return false;
-				//}
-
 				if (elementField.Reference.Target == null)
 				{
 					UpdateStatusText("No element provided!");
 					return false;
 				}
-
-				//if (processWorkerSyncMembers.Value && elementField.Reference.Target is not Worker)
-				//{
-				//	UpdateStatusText("Provided element is not a Worker type!");
-				//	return false;
-				//}
 
 				if (performingOperations)
 				{
@@ -163,44 +136,6 @@ namespace ReferenceFinder
 				return true;
 			}
 
-			//bool IsComponentMatch(Component c)
-			//{
-			//	bool matchType, matchName;
-			//	string compName, searchString;
-
-			//	if (ignoreGenericTypes.Value.Value)
-			//	{
-			//		matchType = c.GetType().Name == componentField.Reference.Target?.GetType().Name;
-			//	}
-			//	else
-			//	{
-			//		matchType = c.GetType() == componentField.Reference.Target?.GetType();
-			//	}
-
-			//	compName = searchNiceName.Value.Value ? c.GetType().GetNiceName() : c.GetType().Name;
-			//	compName = matchCase.Value.Value ? compName : compName.ToLower();
-
-			//	searchString = matchCase.Value.Value ? nameField.Value.Value : nameField.Value.Value?.ToLower();
-
-			//	matchName = searchString != null &&
-			//				searchString.Trim() != "" &&
-			//				(exactMatch.Value.Value ? compName == searchString.Trim() : compName.Contains(searchString.Trim()));
-
-			//	if (componentField.Reference.Target == null || nameField.Value.Value == null || nameField.Value.Value.Trim() == "")
-			//	{
-			//		return matchType || matchName;
-			//	}
-			//	else
-			//	{
-			//		return matchType && matchName;
-			//	}
-			//}
-
-			//List<Component> GetSearchComponents()
-			//{
-			//	return searchRoot.Reference.Target?.GetComponentsInChildren((Component c) => IsComponentMatch(c));
-			//}
-
 			ReferenceFinderWizard(Slot x)
 			{
 				WizardSlot = x;
@@ -209,8 +144,6 @@ namespace ReferenceFinder
 				WizardSlot.LocalScale *= 0.0008f;
 
 				Slot Data = WizardSlot.AddSlot("Data");
-				//searchRoot = Data.AddSlot("searchRoot").AttachComponent<ReferenceField<Slot>>();
-				//searchRoot.Reference.Value = WizardSlot.World.RootSlot.ReferenceID;
 				elementField = Data.AddSlot("elementField").AttachComponent<ReferenceField<IWorldElement>>();
 				processWorkerSyncMembers = Data.AddSlot("processWorkerSyncMembers").AttachComponent<ValueField<bool>>();
 				processWorkerSyncMembers.Value.Value = true;
@@ -221,18 +154,11 @@ namespace ReferenceFinder
 				ignoreNonPersistent = Data.AddSlot("ignoreNonPersistent").AttachComponent<ValueField<bool>>();
 				ignoreSelfReferences = Data.AddSlot("ignoreSelfReferences").AttachComponent<ValueField<bool>>();
 				ignoreSelfReferences.Value.Value = true;
-				ignoreSlotParentRef = Data.AddSlot("ignoreSlotParentRef").AttachComponent<ValueField<bool>>();
-				//ignoreGenericTypes = Data.AddSlot("ignoreGenericTypes").AttachComponent<ValueField<bool>>();
+				//ignoreSlotParentRef = Data.AddSlot("ignoreSlotParentRef").AttachComponent<ValueField<bool>>();
 				showDetails = Data.AddSlot("showDetails").AttachComponent<ValueField<bool>>();
-				//confirmDestroy = Data.AddSlot("confirmDestroy").AttachComponent<ValueField<bool>>();
-				//nameField = Data.AddSlot("nameField").AttachComponent<ValueField<string>>();
-				//matchCase = Data.AddSlot("matchCase").AttachComponent<ValueField<bool>>();
-				//allowChanges = Data.AddSlot("allowChanges").AttachComponent<ValueField<bool>>();
-				//searchNiceName = Data.AddSlot("searchNiceName").AttachComponent<ValueField<bool>>();
 				maxResults = Data.AddSlot("maxResults").AttachComponent<ValueField<int>>();
 				maxResults.Value.Value = 256;
 				results = Data.AddSlot("results").AttachComponent<ReferenceMultiplexer<ISyncRef>>();
-				//exactMatch = Data.AddSlot("exactMatch").AttachComponent<ValueField<bool>>();
 
 				UIBuilder UI = RadiantUI_Panel.SetupPanel(WizardSlot, WIZARD_TITLE.AsLocaleKey(), new float2(800f, 756f));
 				RadiantUI_Constants.SetupEditorStyle(UI);
@@ -255,12 +181,6 @@ namespace ReferenceFinder
 				UI.Style.PreferredWidth = 400f;
 				UI.Style.MinWidth = 400f;
 
-				//UI.Text("Search Root:").HorizontalAlign.Value = TextHorizontalAlignment.Left;
-				//UI.Next("Root");
-				//UI.Current.AttachComponent<RefEditor>().Setup(searchRoot.Reference);
-
-				//UI.Spacer(24f);
-
 				UI.Text("Element:").HorizontalAlign.Value = TextHorizontalAlignment.Left;
 				UI.Next("Element");
 				UI.Current.AttachComponent<RefEditor>().Setup(elementField.Reference);
@@ -273,26 +193,15 @@ namespace ReferenceFinder
 
 				UI.HorizontalElementWithLabel("Ignore references contained within self:", 0.942f, () => UI.BooleanMemberEditor(ignoreSelfReferences.Value));
 
-				UI.HorizontalElementWithLabel("Ignore slot parent references:", 0.942f, () => UI.BooleanMemberEditor(ignoreSlotParentRef.Value));
+				//UI.HorizontalElementWithLabel("Ignore slot parent references:", 0.942f, () => UI.BooleanMemberEditor(ignoreSlotParentRef.Value));
 
 				UI.HorizontalElementWithLabel("Ignore non persistent references:", 0.942f, () => UI.BooleanMemberEditor(ignoreNonPersistent.Value));
 
 				UI.Spacer(24f);
 
-				//UI.Text("{B} Name Contains:").HorizontalAlign.Value = TextHorizontalAlignment.Left;
-
-				//var textField = UI.TextField();
-				//textField.Text.Content.OnValueChange += (field) => nameField.Value.Value = field.Value;
-
-				//UI.HorizontalElementWithLabel("Search Nice Name (With type arguments):", 0.942f, () => UI.BooleanMemberEditor(searchNiceName.Value));
-				//UI.HorizontalElementWithLabel("Match Case:", 0.942f, () => UI.BooleanMemberEditor(matchCase.Value));
-				//UI.HorizontalElementWithLabel("Exact Match:", 0.942f, () => UI.BooleanMemberEditor(exactMatch.Value));
-
-				//UI.Spacer(24f);
-
 				UI.HorizontalElementWithLabel("Max Results:", 0.884f, () =>
 				{
-					var intField = UI.IntegerField(1, 1025);
+					var intField = UI.IntegerField(1, 1024);
 					intField.ParsedValue.Value = maxResults.Value.Value;
 					intField.ParsedValue.OnValueChange += (field) => maxResults.Value.Value = field.Value;
 					return intField;
@@ -302,23 +211,6 @@ namespace ReferenceFinder
 
 				searchButton = UI.Button("Search");
 				searchButton.LocalPressed += SearchPressed;
-
-				//UI.Text("----------");
-
-				//UI.HorizontalElementWithLabel("Allow Changes:", 0.942f, () => UI.BooleanMemberEditor(allowChanges.Value));
-
-				//enableButton = UI.Button("Enable");
-				//enableButton.LocalPressed += EnablePressed;
-
-				//disableButton = UI.Button("Disable");
-				//disableButton.LocalPressed += DisablePressed;
-
-				//UI.Spacer(24f);
-
-				//UI.HorizontalElementWithLabel("Confirm Destroy:", 0.942f, () => UI.BooleanMemberEditor(confirmDestroy.Value));
-
-				//destroyButton = UI.Button("Destroy");
-				//destroyButton.LocalPressed += DestroyPressed;
 
 				UI.Spacer(24f);
 
